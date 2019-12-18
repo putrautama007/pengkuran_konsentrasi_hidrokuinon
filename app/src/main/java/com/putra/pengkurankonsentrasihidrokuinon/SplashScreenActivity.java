8875d54d9c2f56a142146c4727bc3eab15c649f5
putrautama007 <putrautama30@gmail.com>
2019-12-18 13:11:24 +0700

    add splashscreen

--- app/src/main/java/com/putra/pengkurankonsentrasihidrokuinon/SplashScreenActivity.java
+++ app/src/main/java/com/putra/pengkurankonsentrasihidrokuinon/SplashScreenActivity.java
@@ -2,13 +2,27 @@ package com.putra.pengkurankonsentrasihidrokuinon;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    private static final int LOADING_TIME = 2000;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent home=new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },LOADING_TIME);
     }
 }
